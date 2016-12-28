c7cc913b9e06b974ac0b4013a2850a5df21e5696
Ricky Nguyễn <hoann@outlook.com>
2016-12-28 21:23:25 +0700

    cm 28/12

--- Ostrovok2Be/Models/getGeneral/RoomGroup.cs
+++ Ostrovok2Be/Models/getGeneral/RoomGroup.cs
@@ -8,7 +8,7 @@ namespace Ostrovok2Be.Models.getFromOstrovok
 {
    public class RoomGroup
     {
-     public  amenity amenities { get; set; }
+    /* public  amenity amenities { get; set; }*/
      public List<image_roomgroup> image_list_tmpl { get; set; }
      public int? room_group_id { get; set; }
      public string name { get; set; }
